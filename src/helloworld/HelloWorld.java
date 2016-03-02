61e36e8e38d989b223a7b1a1e545053671b27f98
kisay97 <kisay97@naver.com>
2016-03-02 21:48:56 +0900

    21시48분

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World");
+	}
+}
